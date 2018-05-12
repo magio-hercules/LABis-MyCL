805d490acca41a92217edd1b58d2f1400572c609
EVOL <evollove@nate.com>
2018-05-12 10:56:22 +0900

    UI 수정
    item 수정

--- Android/app/src/main/java/com/labis/mycl/contents/RecyclerViewAdapter.java
+++ Android/app/src/main/java/com/labis/mycl/contents/RecyclerViewAdapter.java
@@ -34,7 +34,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewHolder
     // 필수 오버라이드 : View 생성
     @Override
     public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item2, parent, false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
         mContext = parent.getContext();
 
         RecyclerViewHolder holder = new RecyclerViewHolder(v);
