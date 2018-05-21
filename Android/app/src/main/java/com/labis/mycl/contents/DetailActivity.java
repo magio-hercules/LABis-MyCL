d65c3c341cbcb135cda91436b413b6bab135addc
EVOL <evollove@nate.com>
2018-05-21 21:33:33 +0900

    상세보기 작업 업데이트
    상세보기 작업 업데이트

--- Android/app/src/main/java/com/labis/mycl/contents/DetailActivity.java
+++ Android/app/src/main/java/com/labis/mycl/contents/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.labis.mycl.contents;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
@@ -13,6 +14,9 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.labis.mycl.R;
@@ -28,6 +32,8 @@ import butterknife.OnTouch;
 
 public class DetailActivity extends AppCompatActivity {
 
+    private static final String TAG = "DetailActivity";
+
     @BindView(R.id.detail_appbar)
     AppBarLayout appBar;
 
@@ -37,13 +43,45 @@ public class DetailActivity extends AppCompatActivity {
     @BindView(R.id.detail_plus_btn)
     TextView plusBtn;
 
+    @BindView(R.id.detail_chapter_div)
+    LinearLayout detailChapterDiv;
     @BindView(R.id.detail_chapter)
     TextView chapterView;
 
-    private static int index = 0;
+    @BindView(R.id.detail_title)
+    TextView detailTitle;
+
+    @BindView(R.id.detail_org_title)
+    TextView detailOrgTitle;
+
+    @BindView(R.id.detail_genre)
+    TextView detailGenre;
+
+    @BindView(R.id.detail_season_div)
+    LinearLayout detailSeasonDiv;
+    @BindView(R.id.detail_season)
+    TextView detailSeason;
+
+    @BindView(R.id.detail_theater_div)
+    LinearLayout detailTheaterDiv;
+    @BindView(R.id.detail_switch_theater)
+    Switch detailTheater;
+
+    @BindView(R.id.detail_summmary_div)
+    LinearLayout detailSummmaryDiv;
+    @BindView(R.id.detail_summary)
+    TextView detailSummary;
+
+    @BindView(R.id.deatal_ad_image)
+    ImageView imgView;
+
+
+    private static int chapterIndex = 0;
 
     private boolean breakFlag = false;
     private Handler mHandler = new Handler ();
+
+    ImageView backImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,15 +107,60 @@ public class DetailActivity extends AppCompatActivity {
             }
         });
 
-        String imageUrl = item.image;
-        imageUrl = imageUrl.replace("/resize/", "/images/");
-        loadAppTitleImage(imageUrl);
-
-
         minusBtn.setOnTouchListener(mTouchEvent);
         plusBtn.setOnTouchListener(mTouchEvent);
 
+       //
+
+        backImg = new ImageView(this);
+        String imageUrl = item.image;
+        imageUrl = imageUrl.replace("/resize/", "/images/");
+        Picasso.get().load(imageUrl).into(imgView);
+
+        inflateContent(item);
     }
+
+    private void inflateContent(Content Item) {
+        // 챕터
+        if(Item.chapter > 0) {
+            detailChapterDiv.setVisibility(View.VISIBLE);
+            chapterIndex = Item.chapter;
+            chapterView.setText(String.valueOf(chapterIndex) + " 화");
+        }
+
+        // 타이틀
+        detailTitle.setText(Item.name);
+        detailOrgTitle.setText(Item.name_org);
+
+        // 장르
+        detailGenre.setText(ContentsActivity.genreMap.get(Item.gen_id));
+
+        // 즐겨찾기
+
+        // 시즌
+        if(Item.season > 0) {
+            detailSeasonDiv.setVisibility(View.VISIBLE);
+            detailSeason.setText(String.valueOf(Item.season));
+        }
+
+        // 극장판
+        if(Item.theatrical > 0) {
+            detailTheaterDiv.setVisibility(View.VISIBLE);
+            detailTheater.setChecked(true);
+        }
+
+        // 줄거리
+        if(Item.summary != null && Item.summary.length() > 0) {
+            detailSummmaryDiv.setVisibility(View.VISIBLE);
+            detailSummary.setText(Item.summary);
+        }
+
+        //Drawable d = new BitmapDrawable(getResources(), bitmap);
+
+        appBar.setBackground(imgView.getBackground());
+
+    }
+
 
     private View.OnTouchListener mTouchEvent = new View.OnTouchListener() {
             @Override
@@ -104,8 +187,8 @@ public class DetailActivity extends AppCompatActivity {
             Runnable plusAction = new Runnable() {
                 @Override
                 public void run() {
-                    index++;
-                    chapterView.setText(String.valueOf(index) + " 화");
+                    chapterIndex++;
+                    chapterView.setText(String.valueOf(chapterIndex) + " 화");
                     mHandler.postDelayed(this, 100);
                 }
             };
@@ -113,27 +196,31 @@ public class DetailActivity extends AppCompatActivity {
             Runnable minusAction = new Runnable() {
                 @Override
                 public void run() {
-                    index--;
-                    chapterView.setText(String.valueOf(index) + " 화");
+                    chapterIndex--;
+                    chapterView.setText(String.valueOf(chapterIndex) + " 화");
                     mHandler.postDelayed(this, 100);
                 }
             };
         };
 
-    private  void loadAppTitleImage(String url) {
+    private  void loadAppTitleImage(String url, final Content item) {
+        final ProgressDialog progressDoalog = new ProgressDialog(this);
+        progressDoalog.setMessage("잠시만 기다리세요....");
+        progressDoalog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
         Target t = new Target() {
             @Override
             public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
                 Drawable d = new BitmapDrawable(getResources(), bitmap);
                 appBar.setBackground(d);
+                progressDoalog.dismiss();
             }
             @Override
             public void onBitmapFailed(Exception e, Drawable errorDrawable) {
-
+                progressDoalog.dismiss();
             }
             @Override
             public void onPrepareLoad(Drawable placeHolderDrawable) {
-
+                progressDoalog.show();
             }
         };
         Picasso.get().load(url).into(t);
